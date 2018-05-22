0443d646308b725ac1ac63af5026dabca1571de1
garrettwong <gwong005@gmail.com>
2018-05-22 13:31:41 -0700

    combination generator

--- combination_generator.py
+++ combination_generator.py
@@ -2,11 +2,23 @@
 
 import itertools
 
-elements = [1, 2, 3]
-for L in range(0, len(elements)+1):
-    for subset in itertools.combinations(elements, L):
-        print(subset)
-        print(":")
+def convert_tuple_to_ss_string(tup):
+    ret = ""
 
-        for i in subset:
-            print(str(i) + " " )
+    for i in tup:
+        ret += str(i) + " "
+
+    return ret
+
+def run_test():
+    elements = [1, 2, 3]
+    for L in range(0, len(elements)+1):
+        for subset in itertools.combinations(elements, L):
+            print(subset)
+
+            ret = convert_tuple_to_ss_string(subset)
+            print(ret)
+
+
+## test harness
+run_test()
